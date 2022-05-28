462967cc31cad2f98c40311eb4f72c5585ddf659
rafael <rafaelbordoni00@gmail.com>
2022-05-28 03:50:03 -0300

    Update bottom menu and habits page

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -2,21 +2,141 @@ import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
-import { ThreeDots } from "react-loader-spinner";
+import { RotatingLines, ThreeDots } from "react-loader-spinner";
 
 import UserContext from "../shared/UserContext";
+import ErrorMessage from "../shared/ErrorMessage";
 
 import Header from "./Header";
 import Menu from "./Menu";
 
 export default function Habits () {
 
+    const [habits, setHabits] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [loadingHabits, setLoadingHabits] = useState("loading");
+    const [addingHabit, setAddingHabit] = useState("");
+
+    const navigate = useNavigate();
     const { user } = useContext(UserContext);
+
+    useEffect(() => {
+        if (Object.keys(user).length > 0) {
+            axios.get(
+                "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits",
+                { headers: {
+                    Authorization: `Bearer ${user.token}`
+                }}
+            )
+                .then(response => {
+                    setHabits(response.data);
+                    setLoadingHabits("");
+                })
+                .catch(error => {
+                    setErrorMessage(error.response.data.message);
+                    setLoadingHabits("");
+                });
+        }
+    }, [user]);
+
+    if (loadingHabits) {
+        return (
+            <>
+                <Header />
+                <SpinnerContainer>
+                    <RotatingLines width="200" strokeColor="var(--maincolor)" />
+                </SpinnerContainer>
+            </>
+        );
+    }
 
     return (
         <>
             <Header />
+            <Container>
+                <ErrorMessage error={errorMessage} />
+                <HabitsTop>
+                    <TitleAndAddButton>
+                        <h1>Meus hábitos</h1>
+                        <button><ion-icon name="add-outline"></ion-icon></button>
+                    </TitleAndAddButton>
+                    {habits.length === 0 ? (
+                        <NoHabitsText>
+                            Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
+                        </NoHabitsText>
+                    ) : (
+                        <></>
+                    )}
+                </HabitsTop>
+            </Container>
             <Menu />
         </>
     );
 }
+
+const Container = styled.div`
+    position: absolute;
+    top: 72px;
+    bottom: 72px;
+    left: 0px;
+    right: 0px;
+
+    padding: 32px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const HabitsTop = styled.div`
+    width: 100%;
+`;
+
+const TitleAndAddButton = styled.div`
+    width: 100%;
+    height: 36px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1 {
+        color: var(--sec1color);
+        font-family: var(--scriptfont);
+        font-size: 24px;
+    }
+
+    button {
+        width: 36px;
+        height: 36px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        background-color: var(--maincolor);
+        border: 1px solid transparent;
+        border-radius: 4px;
+        cursor: pointer;
+
+        ion-icon {
+            color: var(--divcolor);
+            font-size: 28px;
+            --ionicon-stroke-width: 64px;
+        }
+    }
+`;
+
+const NoHabitsText = styled.p`
+    margin-top: 32px;
+
+    font-family: var(--scriptfont);
+    font-size: 18px;
+`;
+
+const SpinnerContainer = styled.div`
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,42 +1,94 @@
-import React from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import styled from "styled-components";
+import { ThreeDots } from "react-loader-spinner";
 import { CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
 
-export default function Menu ({ habits }) {
+import UserContext from "../shared/UserContext";
+import ErrorMessage from "../shared/ErrorMessage";
+
+export default function Menu () {
+
+    const [daily, setDaily] = useState([]);
+    const [loadingDaily, setLoadingDaily] = useState("loading");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const navigate = useNavigate();
+    const { user } = useContext(UserContext);
+
+    useEffect(() => {
+        if (Object.keys(user).length > 0) {
+            axios.get(
+                "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today",
+                { headers: {
+                    Authorization: `Bearer ${user.token}`
+                }}
+            )
+                .then(response => {
+                    setDaily(response.data);
+                    setLoadingDaily("");
+                })
+                .catch(() => {
+                    setErrorMessage("Erro!");
+                    setLoadingDaily("");
+                });
+        }
+    }, [user]);
 
     function getPercent () {
         let done = 0;
         let total = 0;
-        habits.forEach(habit => {
+        daily.forEach(habit => {
             total++;
             done += habit.done ? 1 : 0;
         });
         return Math.round(done * 100 / total);
     }
 
+    function MidCircle () {
+        if (loadingDaily) {
+            return (
+                <CircContainer>
+                    <div>
+                        <ThreeDots
+                            color="var(--divcolor)"
+                            width="64px"
+                        />
+                    </div>
+                </CircContainer>
+            );
+        } else {
+            return (
+                <CircContainer>
+                    <div>
+                        {errorMessage ? (
+                            <ErrorMessage error={errorMessage} />
+                        ) : (
+                            <CircularProgressbarWithChildren
+                                value={getPercent()}
+                                background
+                                backgroundPadding={6}
+                                styles={buildStyles({
+                                    backgroundColor: "var(--maincolor)",
+                                    textColor: "var(--divcolor)",
+                                    pathColor: "var(--divcolor)",
+                                    strokeLinecap: "round",
+                                    trailColor: "transparent"
+                                })}
+                            >
+                                <p>Hoje</p>
+                            </CircularProgressbarWithChildren>
+                        )}
+                    </div>
+                </CircContainer>
+            );
+        }
+    }
+
     return (
         <>
-            <CircContainer>
-                <div>
-                    <CircularProgressbarWithChildren
-                        value={70}
-                        background
-                        backgroundPadding={6}
-                        styles={buildStyles({
-                            backgroundColor: "var(--maincolor)",
-                            textColor: "var(--divcolor)",
-                            pathColor: "var(--divcolor)",
-                            strokeLinecap: "round",
-                            trailColor: "transparent"
-                        })}
-                    >
-                        <p>Hoje</p>
-                    </CircularProgressbarWithChildren>
-                </div>
-            </CircContainer>
+            <MidCircle />
             <Container>
                 <p onClick={() => navigate("/habitos")}>
                     Hábitos
@@ -87,6 +139,10 @@ const CircContainer = styled.div`
         height: 92px;
         margin-bottom: 16px;
         z-index: 2;
+
+        background-color: var(--maincolor);
+        border: 1px solid transparent;
+        border-radius: 50%;
     }
 
     p {
